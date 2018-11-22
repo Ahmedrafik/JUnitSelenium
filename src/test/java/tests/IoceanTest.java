a32b7e3888ce4108a142dd0fe828123380897355
RAFIK Ahmed <arafik@PC-ARA-L.IOCEANNT.FR>
2018-11-22 10:21:40 +0100

    Premier test selenium avec JUnit

---
+++ src/test/java/tests/IoceanTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import environment.EnvironmentManager;
+import environment.RunEnvironment;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class IoceanTest {
+  private WebDriver driver;
+
+  @Before
+  public void setUp() {
+      EnvironmentManager.initWebDriver();
+      driver = RunEnvironment.getWebDriver();
+  }
+
+  @Test
+  public void test2() {
+      driver.get("http://wikipedia.fr/index.php");
+      driver.findElement(By.id("Search")).clear();
+      driver.findElement(By.id("Search")).sendKeys("software");
+      driver.findElement(By.id("Search")).sendKeys(Keys.ENTER);
+      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Wikiwix'])[1]/following::p[1]")).click();
+      driver.findElement(By.linkText("Brøderbund Software")).click();
+      assertEquals("Brøderbund Software", driver.findElement(By.id("firstHeading")).getText());
+
+  }
+
+  @After
+  public void tearDown() {
+      EnvironmentManager.shutDownDriver();
+  }
+
+}

bbbe35472934aa52c4d412c75301b17e01cfe421
arafik <arafik@iocean.fr>
2018-11-22 16:17:11 +0100

    configuration chrome driver pour jenkins

--- src/test/java/environment/EnvironmentManager.java
+++ src/test/java/environment/EnvironmentManager.java
@@ -1,17 +1,23 @@
 package environment;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class EnvironmentManager {
 
 
-    public static void initWebDriver() {
+    public static void initWebDriver() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER") + "/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("useAutomationExtension", false);
-        WebDriver driver = new ChromeDriver(options);
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:8081/wd/hub"), capability);
+
+//        ChromeOptions options = new ChromeOptions();
+//        options.setExperimentalOption("useAutomationExtension", false);
+//        WebDriver driver = new ChromeDriver(options);
         RunEnvironment.setWebDriver(driver);
     }
 

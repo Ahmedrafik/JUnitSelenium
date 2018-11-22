ffa003cfe43b07c05f2517ef8099ee572d19dfbd
arafik <arafik@iocean.fr>
2018-11-22 16:26:36 +0100

    revert 2 dernier commits

--- src/test/java/environment/EnvironmentManager.java
+++ src/test/java/environment/EnvironmentManager.java
@@ -1,23 +1,17 @@
 package environment;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class EnvironmentManager {
 
 
-    public static void initWebDriver() throws MalformedURLException {
+    public static void initWebDriver() {
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER") + "/chromedriver");
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
-        WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:8081/"), capability);
-
-//        ChromeOptions options = new ChromeOptions();
-//        options.setExperimentalOption("useAutomationExtension", false);
-//        WebDriver driver = new ChromeDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("useAutomationExtension", false);
+        WebDriver driver = new ChromeDriver(options);
         RunEnvironment.setWebDriver(driver);
     }
 

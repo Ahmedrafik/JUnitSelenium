efab8054c4f2171ea1e760288912d70bf7c99614
arafik <arafik@iocean.fr>
2018-11-22 18:12:57 +0100

    modification des option du driver chrome

--- src/test/java/environment/EnvironmentManager.java
+++ src/test/java/environment/EnvironmentManager.java
@@ -11,6 +11,8 @@ public class EnvironmentManager {
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER") + "/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         WebDriver driver = new ChromeDriver(options);
         RunEnvironment.setWebDriver(driver);
     }
